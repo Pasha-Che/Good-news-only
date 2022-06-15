e1069cc2fadf5be03fa44a22c38b2e0dc19a4a79
Pasha Che <pasha-che85@yandex.ru>
2022-06-15 17:57:44 +0300

    release 06

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require('express');
+/* eslint-disable quotes */
+const express = require("express");
 // const session = require('express-session');
 // const FileStore = require('session-file-store')(session);
-const morgan = require('morgan');
-const hbs = require('hbs');
-const path = require('path');
-const cookieParser=require('cookie-parser')
+const morgan = require("morgan");
+const hbs = require("hbs");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-
-const register = require('./routers/register');
-const {checkSession}=requre('./middlewares/checkAuth') 
-const mainPageRouter = require('./routers/mainPageRouter');
+const register = require("./routers/register");
+const { checkSession } = requre("./middlewares/checkAuth");
+const mainPageRouter = require("./routers/mainPageRouter");
 // const registrRouter = require('./routers/registr');
 // const entryRouter = require('./routers/entry');
-
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
+hbs.registerPartials(path.join(process.env.PWD, "views", "partials"));
 
 // const sessionConfig = {
 //   store: new FileStore(),
@@ -32,33 +31,28 @@ hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 //   cookie: { expires: 24 * 60 * 60e3 },
 // };
 // app.use(session(sessionConfig));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/', mainPageRouter);
+app.use("/", mainPageRouter);
 // app.use('/registr', registrRouter);
 // app.use('/entry', entryRouter);
 
-app.use(cookieParser)
-app.use(checkSession)
+app.use(cookieParser);
+app.use(checkSession);
 
+app.use(cookieParser);
+app.use(checkSession);
 
-app.use(cookieParser)
-app.use(checkSession)
-
-app.use('/', mainPageRouter);
+app.use("/", mainPageRouter);
 // app.use('/registr', registrRouter);
 // app.use('/entry', entryRouter);
-
-
 
 const PORT = 3000;
 
-
-app.use('/', registrRouter);
-
+app.use("/", registrRouter);
 
 app.listen(PORT, () => {
-  console.log('vzleteli');
+  console.log("vzleteli");
 });
