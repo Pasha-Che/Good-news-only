3a77375e431f261093fc6a49c93d21f9ad776067
Pasha Che <pashache@MacBook-Air-Pasha.local>
2022-06-15 14:31:01 +0300

    Release 01

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const morgan = require('morgan');
+const hbs = require('hbs');
+const path = require('path');
+
+
+// const indexRouter = require('./routers/index');
+// const registrRouter = require('./routers/registr');
+// const entryRouter = require('./routers/entry');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
+
+const sessionConfig = {
+  store: new FileStore(),
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  httpOnly: true,
+  cookie: { expires: 24 * 60 * 60e3 },
+};
+app.use(session(sessionConfig));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use('/', indexRouter);
+// app.use('/registr', registrRouter);
+// app.use('/entry', entryRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('vzleteli');
+});
