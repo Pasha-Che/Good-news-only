628fff7be5c69aa07d47429d3c403a7019e31727
Ivan <ivan8597@yandex.ru>
2022-06-15 17:07:55 +0200

    auth

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const express = require('express');
 const morgan = require('morgan');
 const hbs = require('hbs');
 const path = require('path');
-const cookieParser=require('cookie-parser')
-
+const cookieParser = require('cookie-parser');
 
 const register = require('./routers/register');
-const {checkSession}=requre('./middlewares/checkAuth') 
+
+const { checkSession } = require('./middlewares/checkAuth');
 const mainPageRouter = require('./routers/mainPageRouter');
 // const registrRouter = require('./routers/registr');
 // const entryRouter = require('./routers/entry');
-
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -22,33 +21,30 @@ app.set('views', path.join(__dirname, 'views'));
 
 hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 
-// const sessionConfig = {
-//   store: new FileStore(),
-//   key: 'sid',
-//   secret: 'secret',
-//   resave: false,
-//   saveUninitialized: false,
-//   httpOnly: true,
-//   cookie: { expires: 24 * 60 * 60e3 },
-// };
-// app.use(session(sessionConfig));
+const sessionConfig = {
+  store: new FileStore(),
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  httpOnly: true,
+  cookie: { expires: 24 * 60 * 60e3 },
+};
+app.use(session(sessionConfig));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(cookieParser)
-app.use(checkSession)
+app.use(cookieParser);
+app.use(checkSession);
 
 app.use('/', mainPageRouter);
 // app.use('/registr', registrRouter);
 // app.use('/entry', entryRouter);
 
-
 const PORT = 3000;
 
-
 app.use('/', registrRouter);
-
 
 app.listen(PORT, () => {
   console.log('vzleteli');
