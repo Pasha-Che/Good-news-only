9a6c3dbf0f81d20eea98659b1c618b2e63e71e9e
Ivan <ivan8597@yandex.ru>
2022-06-16 07:43:56 +0200

    goodsnews

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 /* eslint-disable quotes */
 const express = require("express");
-// const session = require('express-session');
-// const FileStore = require('session-file-store')(session);
+
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 const morgan = require("morgan");
 const hbs = require("hbs");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
-const register = require("./routers/register");
-const { checkSession } = requre("./middlewares/checkAuth");
+const { checkSession } = require("./middleware/checkAuth");
 const mainPageRouter = require("./routers/mainPageRouter");
-// const registrRouter = require('./routers/registr');
-// const entryRouter = require('./routers/entry');
+const registerRouter = require('./routers/registerRouter');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -21,37 +20,27 @@ app.set("views", path.join(__dirname, "views"));
 
 hbs.registerPartials(path.join(process.env.PWD, "views", "partials"));
 
-// const sessionConfig = {
-//   store: new FileStore(),
-//   key: 'sid',
-//   secret: 'secret',
-//   resave: false,
-//   saveUninitialized: false,
-//   httpOnly: true,
-//   cookie: { expires: 24 * 60 * 60e3 },
-// };
-// app.use(session(sessionConfig));
+const sessionConfig = {
+  store: new FileStore(),
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  httpOnly: true,
+  cookie: { expires: 24 * 60 * 60e3 },
+};
+app.use(session(sessionConfig));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 app.use(express.json());
 
+// app.use(cookieParser);
+app.use(checkSession);
 app.use("/", mainPageRouter);
-// app.use('/registr', registrRouter);
+app.use('/register', registerRouter);
 // app.use('/entry', entryRouter);
 
-app.use(cookieParser);
-app.use(checkSession);
-
-app.use(cookieParser);
-app.use(checkSession);
-
-app.use("/", mainPageRouter);
-// app.use('/registr', registrRouter);
-// app.use('/entry', entryRouter);
-
-const PORT = 3000;
-
-app.use("/", registrRouter);
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log("vzleteli");
